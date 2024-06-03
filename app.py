17da46e9c2f106150cef81abb3becdbadd64a18f
raphaelvmclima <raphaelvmclima@gmail.com>
2024-06-02 23:35:30 -0300

    att

--- app.py
+++ app.py
@@ -33,8 +33,7 @@ def authorize():
 def oauth2callback():
     state = session['state']
     flow = InstalledAppFlow.from_client_secrets_file(
-        'credentials.json', SCOPES,
-        state=state,
+        'credentials.json', SCOPES, state=state,
         redirect_uri=REDIRECT_URI)
     flow.fetch_token(authorization_response=request.url)
 
